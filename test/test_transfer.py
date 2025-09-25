69ffb2d9b50d972ff59bc338ff7a60a0f8d4a6de
Manayer <manayerbusiness@gmail.com>
2025-09-25 11:45:15 +0300

    test transfer from to another customer

--- test/test_transfer.py
+++ test/test_transfer.py
@@ -26,6 +26,19 @@ class TestBankTransfer(unittest.TestCase):
         self.assertEqual(self.cus1.save, init_save - 100)
         self.assertEqual(self.cus1.check, init_check +100)
         
+    def test_transfer_to_other_customer(self):
+        init_check_1 = self.cus1.check
+        init_check_2 = self.cus2.check
+        self.tran1.transfer("checking","checking",300,target_customer=self.cus2)
+        self.assertEqual(self.cus1.check, init_check_1 -300)
+        self.assertEqual(self.cus2.check,init_check_2 +300)
+    def test_transfer_overTheLimit(self):
+        init_check = self.cus1.check
+        self.tran1.transfer("checking","savings" , 5000)
+        self.assertEqual(self.cus1.check, init_check)
+        
+        
+        
 if __name__ == "__main__":
     unittest.main()
         
